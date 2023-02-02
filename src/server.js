2fa0356a8e8becec44c76d157a31c190706db573
Iann Rodrigues <iannrst11@gmail.com>
2023-02-02 19:07:36 -0300

    feat: Servidor

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-/* Servidor */
+import http from 'node:http';
+import { routes } from './routes.js';
+
+
+
+const server = http.createServer((req, res)=>{
+  const {method, url} = req;
+  
+  const route = routes.find(route => {
+    return route.method===method && route.path===url
+  });
+
+  if(route){
+    return route.handler(req, res)
+  }
+});
+
+server.listen(3335);
